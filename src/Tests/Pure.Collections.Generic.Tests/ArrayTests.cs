0a8d2587490baf397ed7851a28a51dea9f01effc
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-06 17:32:12 +0300

    Implemented tests for Array<T> collection type.

---
+++ src/Tests/Pure.Collections.Generic.Tests/ArrayTests.cs
@@ -0,0 +1,35 @@
+namespace Pure.Collections.Generic.Tests;
+
+public sealed record ArrayTests
+{
+    [Fact]
+    public void InitializeCorrectly()
+    {
+        Random random = new Random();
+        IReadOnlyCollection<int> randomNumbers =
+        [
+            .. Enumerable.Range(1, 100).Select(_ => random.Next()),
+        ];
+        Assert.Equal(randomNumbers, new Array<int>(randomNumbers));
+    }
+
+    [Fact]
+    public void UnderlyingTypeIsArray()
+    {
+        using IEnumerator<int> enumerator = new Array<int>([]).GetEnumerator();
+
+        Assert.Equal("SZGenericArrayEnumerator`1", enumerator.GetType().Name);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        _ = Assert.Throws<NotSupportedException>(() => new Array<int>([]).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        _ = Assert.Throws<NotSupportedException>(() => new Array<int>([]).ToString());
+    }
+}
